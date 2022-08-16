55e49337af063b4fe7b325838147a5531cbe4332
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-08-16 11:52:09 +0200

    Tweak unicorn/prefer-export-from

--- packages/base/index.js
+++ packages/base/index.js
@@ -68,6 +68,7 @@ module.exports = {
     'unicorn/no-null': 'off',
     'unicorn/no-process-exit': 'off',
     'unicorn/no-useless-undefined': 'off',
+    'unicorn/prefer-export-from': ['error', { ignoreUsedVariables: true }],
     'unicorn/prefer-module': 'off',
     'unicorn/prevent-abbreviations': 'off',
   },
