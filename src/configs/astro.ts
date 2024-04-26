36ad80e81df0e8268fdcd36cf2f5d9ea090954a6
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-04-26 14:14:52 +0200

    chore: fix type issue

--- src/configs/astro.ts
+++ src/configs/astro.ts
@@ -29,7 +29,9 @@ export function astro(): Config[] {
       },
       processor: 'astro/client-side-ts',
       rules: {
+        // @ts-expect-error - This is not properly typed in the plugin but the rules exist.
         ...pluginAstro.configs.recommended.rules,
+        // @ts-expect-error - This is not properly typed in the plugin but the rules exist.
         ...pluginAstro.configs['jsx-a11y-recommended'].rules,
         ...pluginTypeScript.configs.disableTypeChecked.rules,
 
